1dc98415a71f02779e4859856a353cc9f4b705a7
NeverSettels <settels.eric@gmail.com>
2020-05-15 11:18:34 -0700

    complete simple UI

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -15,9 +15,27 @@ namespace Program
       }
       Console.WriteLine();
     }
+    public static string Validate()
+    {
+      TypeSlow("Enter a word to count: ");
+      string word = Console.ReadLine();
+      if (RepeatCounter.VerifyWord(word))
+      {
+        return word;
+      }
+      else
+      {
+        TypeSlow("Sorry! that wont work! try again....");
+        return Validate();
+      }
+    }
     public static void Main()
     {
-
+      TypeSlow("Enter a Sentence to count words in: ");
+      string sentence = Console.ReadLine();
+      string word = Validate();
+      int counted = RepeatCounter.CountWords(word, sentence);
+      TypeSlow($"{word} appears {counted} times in that sentence!");
     }
   }
 }
